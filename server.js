6b3766952f815307ebd9b48b6de9910e15331d20
MeetRadadiya1999 <meetradadiya45@gmail.com>
2024-10-03 00:45:41 +0530

    add vercel app url in cors

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
-// Load environment variables from .env file
 dotenv.config();
 
-// Connect to the database
 connectDB();
 
 const app = express();
 
-// Middleware
-app.use(express.json()); // Parse incoming JSON requests
-app.use(cors()); // Enable Cross-Origin Resource Sharing
+app.use(express.json()); 
 
-// Add a root route
+app.use(cors({
+  origin: 'https://web-story-5kzh5kjgx-meetradadiya1999s-projects.vercel.app/'
+}));
+
+
 app.get('/', (req, res) => {
-  res.send('Welcome to my API!'); // Simple response
+  res.send('Welcome to my API!');
 });
 
 // Routes
@@ -25,16 +25,13 @@ app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/stories', require('./routes/storyRoutes'));
 app.use('/api/slides', require('./routes/slideRoutes'));
 
-// Error handling middleware (optional, improves error management)
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Server Error', error: err.message });
 });
 
-// Set the port
 const PORT = process.env.PORT || 5000;
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 });
